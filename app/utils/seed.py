81746d29731628c13dee5fac1e2eb5dafe0d35ff
J-A-Gr <jonasgrocas@gmail.com>
2025-05-29 13:52:52 +0300

    Refactor user registration form and update seed functions for hardcoded users

--- app/utils/seed.py
+++ app/utils/seed.py
@@ -25,3 +25,53 @@ def create_hardcoded_admin():
         print("Hardcoded admin created.")
     else:
         print("Hardcoded admin already exists.")
+###################################################################################################
+# for test purposes, leave commented out.
+
+# def create_hardcoded_student():
+#     inspector = inspect(db.engine)
+
+#     if 'users' not in inspector.get_table_names():
+#         print("'users' table does not exist. Skipping hardcoded admin creation.")
+#         return
+
+#     if not User.query.filter_by(email='student@example.com').first():
+#         admin = User(
+#             email='student@example.com',
+#             password_hash=bcrypt.generate_password_hash('Admin123+').decode('utf-8'),
+#             is_student=True,
+#             first_name = 'Juonis',
+#             last_name = 'Hardkodas',
+#             is_active=True,
+#             created_at=datetime.utcnow(),
+#             updated_at=datetime.utcnow()
+#         )
+#         db.session.add(admin)
+#         db.session.commit()
+#         print("Hardcoded student created.")
+#     else:
+#         print("Hardcoded student already exists.")
+        
+# def create_hardcoded_teacher():
+#     inspector = inspect(db.engine)
+
+#     if 'users' not in inspector.get_table_names():
+#         print("'users' table does not exist. Skipping hardcoded admin creation.")
+#         return
+
+#     if not User.query.filter_by(email='teacher@example.com').first():
+#         admin = User(
+#             email='teacher@example.com',
+#             password_hash=bcrypt.generate_password_hash('Admin123+').decode('utf-8'),
+#             is_teacher=True,
+#             first_name = 'Jan',
+#             last_name = 'Ivanas',
+#             is_active=True,
+#             created_at=datetime.utcnow(),
+#             updated_at=datetime.utcnow()
+#         )
+#         db.session.add(admin)
+#         db.session.commit()
+#         print("Hardcoded teacher created.")
+#     else:
+#         print("Hardcoded teacher already exists.")
